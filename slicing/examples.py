997a5c3e7f6f014a85664ea8245bc4f9acf08a88
Hugo Arevalo <gohulu@gmail.com>
2023-06-28 13:28:53 -0500

    Slicing examples

---
+++ slicing/examples.py
@@ -0,0 +1,10 @@
+word = "movie"
+sliced = word[:2]
+print(sliced)
+
+sliced = word[-1]
+print(sliced)
+
+sliced = word[::2]
+print(sliced)
+
---
+++ slicing/reverse.py
@@ -0,0 +1,4 @@
+my_string = "ferrari"
+my_string_reversed = my_string[::-1]
+print(my_string)
+print(my_string_reversed)
